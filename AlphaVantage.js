2c5d930bde1bffb8f062b31fa65ce19f29b2b31c
SpencerD1 <46722292+SpencerD1@users.noreply.github.com>
2020-04-14 15:05:12 -0400

    Update AlphaVantage.js

---
+++ AlphaVantage.js
@@ -0,0 +1,14 @@
+class AlphaVantage {
+    // class methods
+    constructor() {
+        console.log("Creating AlphaVantage");
+    }
+
+    getStock(stock){
+        console.log("AV: getting: " + stock);
+    }
+
+    getStockList(stockList){
+        console.log("AV: getting: " + stockList);
+    }
+  }
